50bc337703f2c93696d41583af5aa60d799f952b
severine <127178206+sev-d@users.noreply.github.com>
2024-02-21 00:45:00 +1030

    Added articles routes for display

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,18 @@ import Blog from "./pages/Blog.jsx";
 import CreateBlog from "./pages/CreateBlog.jsx";
 import BlogDetails from "./pages/BlogDetails.jsx";
 import Events from "./pages/Events.jsx";
-import WhatIsAutism from "./components/WhatIsAutism/WhatIsAutismPage.jsx"; 
-
+import WhatIsAutism from "./components/WhatIsAutism/WhatIsAutismPage.jsx"; // Import the new component
+import AutismSpectrumArticle from "./articles/AutismSpectrumArticle.jsx"; // Import the new component
+import WhatIsAutismArticle from "./articles/WhatIsAutismArticle.jsx"; // Import the new component
+import CharacteristicsOfAutismArticle from "./articles/CharacteristicsOfAutismArticle.jsx"; // Import the new component
+import AspergersSyndromeArticle from "./articles/AspergersSyndromeArticle.jsx"; // Import the new component
+import PositiveSelfIdentityArticle from "./articles/PositiveSelfIdentityArticle.jsx"; // Import the new component
+import NeurodiversityMovementArticle from "./articles/NeurodiversityMovementArticle.jsx"; // Import the new component
+import PrevalenceOfAutismArticle from "./articles/PrevalenceOfAutismArticle.jsx"; // Import the new component
+import HistoryOfAutismArticle from "./articles/HistoryOfAutismArticle.jsx"; // Import the new component
+import CausesOfAutismArticle from "./articles/CausesOfAutismArticle.jsx"; // Import the new component
+import FactsAndMisconceptionsArticle from "./articles/FactsAndMisconceptionsArticle.jsx"; // Import the new component
+import BroachingAutismSubjectArticle from "./articles/BroachingAutismSubjectArticle.jsx"; // Import the new component
 
 function App() {
   return (
@@ -30,6 +40,18 @@ function App() {
           <Route path="/events" element={<Events />} />
           
           <Route path="/what-is-autism" element={<WhatIsAutism />} />
+            <Route path="/what-is-autism-article" element={<WhatIsAutismArticle />} />
+            <Route path="/autism-spectrum-article" element={<AutismSpectrumArticle />} />
+            <Route path="/characteristics-of-autism-article" element={<CharacteristicsOfAutismArticle />} />
+            <Route path="/aspergers-syndrome-article" element={<AspergersSyndromeArticle />} />
+            <Route path="/positive-self-identity-article" element={<PositiveSelfIdentityArticle />} />
+            <Route path="/neurodiversity-movement-article" element={<NeurodiversityMovementArticle />} />
+            <Route path="/prevalence-of-autism-article" element={<PrevalenceOfAutismArticle />} />
+            <Route path="/history-of-autism-article" element={<HistoryOfAutismArticle />} />
+            <Route path="/causes-of-autism-article" element={<CausesOfAutismArticle />} />
+            <Route path="/facts-and-misconceptions-article" element={<FactsAndMisconceptionsArticle />} />
+            <Route path="/broaching-autism-subject-article" element={<BroachingAutismSubjectArticle />} />
+            {/* Add more routes as needed */}
 
         </Routes>
       </BrowserRouter>
