f12f7adb86f5cf76c434f1cf7c8c9cb2e7476900
severine <127178206+sev-d@users.noreply.github.com>
2024-02-24 23:53:11 +1030

    minor change - NavBar path

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { request } from "graphql-request";
 import BlogContent from "../components/Blog/BlogContent";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/NavBar/NavBar"; 
 import WhiteFooter from "../components/WhiteFooter";
+// import { request } from "graphql-request";
 
 const BlogDetails = () => {
   const [post, setPost] = useState(null);
